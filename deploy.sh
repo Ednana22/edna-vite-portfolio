87352c43be221757dd707b5cdf3cf4bb0109c73b
unick_how <unick.zhow@gmail.com>
2021-08-24 00:01:48 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,6 +19,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Ednana22/edna-vite-portfolio.git main:gh-pages
+git push -f git@github.com:Ednana22/edna-vite-portfolio.git master:gh-pages
 
 cd -
