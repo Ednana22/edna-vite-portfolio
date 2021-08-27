4afe6f16ae8b95cd461e97bd34896a495a1db1da
edna <w.ehsien@gmail.com>
2021-08-27 23:32:22 +0800

    homepage fix for section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <section class="hero mt-60">
+    <section class="hero mt-60 border-bottom">
         <div class="container-hero">
             <div class="row hero">
                 <div class="col-md-6 mb-56">
@@ -31,123 +31,120 @@
     </section>
     <section class="market">
         <div class="container">
-        <div class="row project">
-            <div class="col-lg-8 content-pr-48">
-            <div class="market-device-section mb-56">
-                <div class="img-container web gradient-border"></div>
-                <div class="img-container mobile gradient-border"></div>
+            <div class="row project">
+                <div class="col-lg-8 content-pr-48">
+                    <div class="market-device-section mb-56">
+                        <div class="img-container web gradient-border"></div>
+                        <div class="img-container mobile gradient-border"></div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <h3>奧丁丁市集平台改版</h3>
+                    <div class="text-md mb-24">
+                        品牌再造
+                        <span class="font-color-lighter"> | </span>
+                        網站重構
+                        <span class="font-color-lighter"> | </span>
+                        APP規劃
+                        <span class="font-color-lighter"> | </span>
+                        UX研究
+                        <span class="font-color-lighter"> | </span>
+                        UI設計
+                    </div>
+                    <div class="text-md">
+                        為奧丁丁市集重構電子商務網站，並進行品牌再造跨部門合作，以優質生鮮電商幫助在地優質小農推廣用心製造的產品。
+                    </div>
+                    <router-link class="btn btn-link" :to="{ name: 'ProjectMarket' }">
+                        瀏覽專案<i class="las la-long-arrow-alt-right"></i>
+                    </router-link>
+                    <div class="display-on-mobile mt-32">
+                        <figure class="figure gradient-border">
+                        <img
+                            src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/market-web-hp.gif"
+                            class="figure-img img-fluid "
+                            alt="..."
+                        />
+                        </figure>
+                    </div>
+                </div>
             </div>
-            </div>
-            <div class="col-lg-4">
-            <h3>奧丁丁市集平台改版</h3>
-            <div class="text-md mb-24">
-                品牌再造
-                <span class="font-color-lighter"> | </span>
-                網站重構
-                <span class="font-color-lighter"> | </span>
-                APP規劃
-                <span class="font-color-lighter"> | </span>
-                UX研究
-                <span class="font-color-lighter"> | </span>
-                UI設計
-            </div>
-            <div class="display-on-mobile mb-32">
-                <figure class="figure gradient-border">
-                <img
-                    src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/market-web-hp.gif"
-                    class="figure-img img-fluid "
-                    alt="..."
-                />
-                </figure>
-            </div>
-            <div class="text-md">
-                為奧丁丁市集重構電子商務網站，並進行品牌再造跨部門合作，以優質生鮮電商幫助在地優質小農推廣用心製造的產品。
-            </div>
-            <router-link class="btn btn-link" :to="{ name: 'ProjectMarket' }">
-                瀏覽專案<i class="las la-long-arrow-alt-right"></i>
-            </router-link>
         </div>
-        </div>
-    </div>
     </section>
-    <section class="offcial-web">
+    <section class="offcial-web border-top border-bottom">
         <div class="container">
-        <div class="row project">
-            <div class="col-lg-8 content-pr-48">
-            <div class="pms-device-section mb-56">
-                <figure class="figure gradient-border">
-                <img
-                    src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/website-homepage.gif"
-                    class="figure-img img-fluid"
-                    alt="..."
-                />
-                </figure>
+            <div class="row project">
+                <div class="col-lg-8 content-pr-48">
+                    <div class="official-web-device-section mb-56">
+                        <figure class="figure gradient-border">
+                            <img
+                                src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/website-homepage.gif"
+                                class="figure-img img-fluid"
+                                alt="..."/>
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <h3>奧丁丁集團官網重構 </h3>
+                    <div class="text-md mb-24">
+                        網站重構<span class="font-color-lighter"> | </span>
+                        UX研究<span class="font-color-lighter" > | </span>
+                        UI設計<span class="font-color-lighter"> | </span>形象官網
+                    </div>
+                    <div class="text-md">
+                        重構奧丁丁集團官網，將集團理念以及各事業體介紹更清楚的傳達給使用者。跨集團七個事業體進行資訊整合，並從集團VI延伸為網站定義風格。
+                    </div>
+                    <router-link class="btn btn-link" :to="{ name: 'ProjectWeb' }">
+                        瀏覽專案<i class="las la-long-arrow-alt-right"></i>
+                    </router-link>
+                    <div class="display-on-mobile mt-32">
+                        <figure class="figure gradient-border">
+                        <img
+                            src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/website-homepage.gif"
+                            class="figure-img img-fluid"
+                            alt="..."
+                        />
+                        </figure>
+                    </div>
+                </div>
             </div>
-            </div>
-            <div class="col-lg-4">
-            <h3>奧丁丁集團官網重構</h3>
-            <div class="text-md mb-24">
-                網站重構<span class="font-color-lighter"> | </span>
-                UX研究<span class="font-color-lighter" > | </span>
-                UI設計<span class="font-color-lighter"> | </span>形象官網
-            </div>
-            <div class="display-on-mobile mb-32">
-                <figure class="figure gradient-border">
-                <img
-                    src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/website-homepage.gif"
-                    class="figure-img img-fluid"
-                    alt="..."
-                />
-                </figure>
-            </div>
-            <div class="text-md">
-                重構奧丁丁集團官網，將集團理念以及各事業體介紹更清楚的傳達給使用者。跨集團七個事業體進行資訊整合，並從集團VI延伸為網站定義風格。
-
-            </div>
-            <router-link class="btn btn-link" :to="{ name: 'ProjectWeb' }">
-                瀏覽專案<i class="las la-long-arrow-alt-right"></i>
-            </router-link>
-            </div>
-        </div>
         </div>
     </section>    
-    <section class="pms">
+    <section class="pms border-bottom">
         <div class="container">
-        <div class="row project">
-            <div class="col-lg-8 content-pr-48">
-            <div class="pms-device-section">
-                <figure class="figure">
-                <img
-                    src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/pms-homepage.gif"
-                    class="figure-img img-fluid"
-                    alt="..."
-                />
-                </figure>
+            <div class="row project">
+                <div class="col-lg-8 content-pr-48">
+                    <div class="pms-device-section mb-56">
+                        <figure class="figure">
+                            <img
+                                src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/pms-homepage.gif"
+                                class="figure-img img-fluid"
+                                alt="..."/>
+                        </figure>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <h3>旅宿管理系統功能規劃</h3>
+                    <div class="text-md mb-24">
+                        UX研究<span class="font-color-lighter"> | </span>
+                        UI設計<span class="font-color-lighter"> | </span>
+                        B2B
+                    </div>
+                    <div class="text-md">
+                        對於OwlNest旅宿管理系統的鋪價功能進行擴充以及優化，達成便捷快速及可彈性修改的目的。
+                    </div>
+                    <router-link class="btn btn-link" :to="{ name: 'ProjectPMS' }">
+                        瀏覽專案<i class="las la-long-arrow-alt-right"></i>
+                    </router-link>
+                    <div class="display-on-mobile mt-32">
+                        <figure class="figure">
+                            <img
+                                src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/pms-homepage.gif"
+                                class="figure-img img-fluid"
+                                alt="..."/>
+                        </figure>
+                    </div>                    
+                </div>
             </div>
-            </div>
-            <div class="col-lg-4">
-            <h3>旅宿管理系統功能規劃</h3>
-            <div class="text-md mb-24">
-                UX研究<span class="font-color-lighter"> | </span>UI設計<span
-                class="font-color-lighter"> | </span>B2B
-            </div>
-            <div class="display-on-mobile mb-32">
-                <figure class="figure">
-                <img
-                    src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/pms-homepage.gif"
-                    class="figure-img img-fluid"
-                    alt="..."
-                />
-                </figure>
-            </div>
-            <div class="text-md">
-                對於OwlNest旅宿管理系統的鋪價功能進行擴充以及優化，達成便捷快速及可彈性修改的目的。
-            </div>
-            <router-link class="btn btn-link" :to="{ name: 'ProjectPMS' }">
-                瀏覽專案<i class="las la-long-arrow-alt-right"></i>
-            </router-link>
-            </div>
-        </div>
         </div>
     </section>
     <section class="connect-section">
@@ -160,8 +157,7 @@
                                 <img
                                     src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/icon-mail.svg"
                                     class="figure-img img-fluid"
-                                    alt="..."
-                                />
+                                    alt="..."/>
                             </figure>
                             <a href="mailto: w.ehsien@gmail.com">w.ehsien@gmail.com</a>
                         </div>  
@@ -170,8 +166,7 @@
                                 <img
                                     src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/icon-linkedin.svg"
                                     class="figure-img img-fluid"
-                                    alt="..."
-                                />
+                                    alt="..."/>
                             </figure>
                             <a href="https://www.linkedin.com/in/edna-wei-90292b97/" target="_blank">LinkedIn Profile</a>
                         </div>                
@@ -181,20 +176,6 @@
     </section>
     <div class="footer">
         <div class="flex-center">
-            <!-- <figure class="figure mr-24">
-                <img
-                    src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/icon-mail.svg"
-                    class="figure-img img-fluid"
-                    alt="..."
-                />
-            </figure>
-            <figure class="figure">
-                <img
-                    src="https://ednasportfolio.s3.ap-northeast-1.amazonaws.com/homepage/icon-linkedin.svg"
-                    class="figure-img img-fluid"
-                    alt="..."
-                />
-            </figure>         -->
         </div>
         <p class="text-sm text-align-center font-color-light">Copyright © EdnaWei, All Rights Reserved.</p>
     </div>
