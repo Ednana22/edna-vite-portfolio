19b61a82e5437388b9739c6211cf5ca84071cb24
unick_how <unick.zhow@gmail.com>
2021-08-29 22:04:47 +0800

    ci: provide multi-enrty

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
+const { resolve } = require('path')
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/edna-vite-portfolio/'
+  base: '/edna-vite-portfolio/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/entries/about.html'),
+        market: resolve(__dirname, 'src/entries/market.html'),
+        'official-website': resolve(__dirname, 'src/entries/official-website.html'),
+        pms: resolve(__dirname, 'src/entries/pms.html')
+      }
+    }
+  }
 })
