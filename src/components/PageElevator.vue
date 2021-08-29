a3adc357861a3c9fb95c496776117cd6550c2020
unick_how <unick.zhow@gmail.com>
2021-08-29 14:29:34 +0800

    feat: separate style files

--- src/components/PageElevator.vue
+++ src/components/PageElevator.vue
@@ -66,34 +66,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-#page-elevator {
-  position: fixed;
-  z-index: 20;
-  bottom: 1rem;
-  right: 1rem;
-  padding: 0.25rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-  border: 0px;
-  backdrop-filter: blur(5px);
-  background-color: rgba(230, 230, 230, 0.6);
-  a {
-    padding: 0.25rem;
-    cursor: pointer;
-    span {
-      font-weight: normal;
-    }
-    &.is_active {
-      span {
-        font-weight: bold;
-      }
-    }
-  }
-  @media (max-width: 576px) {
-    display: none;
-  }
-}
-</style>
+<style src="../styles/pageElevator.scss"></style>
