eed69492beafc06b172cc1087532f9918be2e8a1
edna <w.ehsien@gmail.com>
2021-10-14 00:03:45 +0800

    adjust: wording

--- src/pages/Projects/PMS.vue
+++ src/pages/Projects/PMS.vue
@@ -26,8 +26,8 @@
       <div class="background mb-48">
         <h2 class="mb-16">專案背景</h2>
         <p class="text-md mb-48">
-          OwlNest 是一款為旅宿業打造的 B2B 三合一整合型訂房系統，提供前台官網訂房系統、PMS 訂單管理系統、線上銷售平台通路串接，用戶遍及亞洲、美洲國家，旅宿用戶達上千家。<br/>
-          因早期 OwlNest 系統客群是鎖定民宿業者，只做到單一房型單一專案的鋪價功能，近幾年隨著市場逐漸拓展，客群也從民宿業者增加至小型旅館業者再加上平台串接功能的開發，<span class="text-md-bold">目前介面功能無法達到有效的鋪設價格</span>，對使用者來說不僅費時也容易出錯。
+          OwlNest 是一款為旅宿業打造的 B2B 三合一整合型訂房系統，提供前台官網訂房系統、PMS 訂單管理系統、線上銷售平台通路串接，用戶遍及亞洲、美洲等國家，旅宿用戶達上千家。<br/>
+          因早期系統客群是鎖定民宿業者，只做到單一房型單一專案的鋪價功能，近幾年隨著市場逐漸拓展，客群也從民宿業者增加至旅館業者再加上平台串接功能的開發，<span class="text-md-bold">目前介面功能無法達到有效的鋪設價格，對使用者來說不僅費時也容易出錯</span>。
         </p>
       </div>
       <div class="task mb-48">
@@ -37,14 +37,14 @@
               <span class="mr-8">•</span>
               <li>
                 <span class="text-md-bold">無法與真實與用者進行訪談</span> -
-                此產品向來都是由業務端帶回使用者反饋並發起優化需求的，因此 UI/UX 設計師很少有機會與真實使用者接觸。為了解決此問題我利用機會訪談了集團旗下的 OwlStay 實體旅宿部門同事，補足了使用者訪談的。
+                此產品向來都是由業務端帶回使用者反饋並發起優化的需求，因此 UI/UX 設計師很少有機會與真實使用者接觸。為了解決此問題我訪談了集團旗下的 OwlStay 實體旅宿部門的同事，瞭解使用者在操作上的真實痛點。
               </li>
             </div>
             <div class="flex">
               <span class="mr-8">•</span>
               <li>
                 <span class="text-md-bold">與產品顧問持不同意見</span> -
-                在討論優化段產品顧問提出想把原有鋪價流程一次翻新的做法，但 UI/UX 端持盡量以不要影響到目前使用者的操作為原則。針對這點我搜集了 NNGroup 裡的文獻作為驗證，並做出簡易 Prototype 以實際操作體驗來說服產品顧問。
+                在討論優化階段產品顧問提出想把原有鋪價流程一次翻新的做法，但 UI/UX 端持盡量以不要影響到目前使用者的操作為原則。針對這點我做出簡易 Prototype 以實際操作體驗來說服產品顧問。
               </li>
             </div>
           </ul>
@@ -115,15 +115,15 @@
       <ul>
         <div class="flex mb-8">
           <span class="mr-8">‣</span>
-          <li class="text-md">進行新設定介面開發，達到更符合使用者期待的鋪價設定</li>
+          <li class="text-md">進行設定介面開發，達到更符合使用者期待的鋪價功能</li>
         </div>
         <div class="flex mb-8">
           <span class="mr-8">‣</span>
-          <li class="text-md">依照現有使用者的反饋來優化現行介面，讓系統中新舊功能共存</li>
+          <li class="text-md">依照現有使用者的反饋來優化現行介面，讓系統中新舊功能並存</li>
         </div>
         <div class="flex">
           <span class="mr-8">‣</span>
-          <li class="text-md">市面上沒有任何一套系統支援針對國定假日鋪價，可將此功能列為獨家賣點</li>
+          <li class="text-md">針對國定假日進行快速且彈性的鋪價</li>
         </div>
       </ul>
     </section>
@@ -140,7 +140,7 @@
             </div>
           </div>
           <p class="text-md">
-            <span class="text-md-bold">對奧丁丁旗下 OwlStay 旅宿團隊進行鋪價習慣訪談，及參考業務帶回來的旅宿業者反饋</span>，得知使用者的鋪價習慣通常是先鋪設一年的平日、假日房價，再鋪設國定假日房價，有些國外旅客較多的旅宿業者甚至會參考鄰國的連假日期去制訂銷售價格。          </p>
+            <span class="text-md-bold">對奧丁丁旗下 OwlStay 旅宿團隊進行鋪價習慣訪談，並參考業務帶回來的旅宿業者反饋</span>，得知使用者的鋪價習慣通常是先鋪設一年的平日、假日房價再鋪設國定假日房價，有些國外旅客較多的旅宿業者甚至會參考鄰國的連假日期去制訂銷售價格。          </p>
         </div>
         <div class="col-md-7 content-pl-48 text-align-center">
           <div class="text-sm-bold plan_border mb-8">平日房價</div>
@@ -166,7 +166,7 @@
             </div>
           </div>
           <p class="text-md">
-            使用者們最常反應的就是目前介面在鋪設<span class="text-md-bold">長區間且多時段的房價時只能一項一項去設定</span>，操作繁雜且重複性高的操作也容易造成鋪價錯誤的情況，先前就有旅宿業者沒有設定到連假價格而造成虧損的問題。          </p>
+            使用者們最常反應的就是目前介面在鋪設<span class="text-md-bold">長區間且多時段的房價時只能一項一項去設定</span>，操作繁雜且重複性高的操作也容易造成鋪價錯誤的情況，先前就有旅宿業者沒有設定到連假價格而造成虧損的問題。</p>
         </div>
         <div class="col-md-7 content-pl-48">
           <figure class="figure border-thin border-radius-4">
@@ -224,7 +224,7 @@
                   <span class="mr-8">•</span>
                   <li>
                     <span class="text-md-bold">國定假日區間選取 - </span>
-                     我們也觀察到雖然目前介面中有提供國定假日的日期資訊，但使用者無法在鋪價時同時看到連假日期，因此使用者通常都是打開人事行政局以另一個視窗來比對，為此<span class="text-md-bold">我們進行下拉日期介面的優化及規劃可複選國定假日的鋪價介面。</span>
+                     我也觀察到雖然目前介面中有提供國定假日的日期資訊，但使用者無法在鋪價時同時看到連假日期，為此<span class="text-md-bold">我們進行下拉日期介面的優化及規劃可複選國定假日的鋪價介面。</span>
                   </li>
                 </div>
               </ul>
@@ -291,7 +291,7 @@
         </div>
         <div class="2mockup row content mb-80">
           <div class="col-md-5 content-pr-48 mb-48">
-            <h5 class="mb-16">II. UI Library & Mockup</h5>
+            <h5 class="mb-16">II. Mockup</h5>
             <p class="text-md">
               後台比起視覺更重視的是使用者完成任務的流程，<span class="text-md-bold">在規劃介面時為避免使用者的學習成本增加</span>，應該遵從既有的介面及流程，並依原有的介面原件快速做出 Mockup 與團隊討論。
             </p>
