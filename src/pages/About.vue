bcee1b9188abb899d96f2a073b5839ee8959e435
edna <w.ehsien@gmail.com>
2021-10-28 23:58:33 +0800

    add: about pade i18n

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -8,10 +8,7 @@
     <section class="about container">
       <h1>Hi! I'm Edna</h1>
       <p class="text-md mb-24">
-        我叫魏依嫻，是一個擁有基礎前端技能並持續學習的 UI/UX 設計師，設計的產品包含 B2C 電商、B2B
-        SasS 產品、形象官網等。工作時總是在功能與美學、通用性與工程實作中找尋最佳平衡。
-        <br>
-        在規劃 UX 的任何一步心中都需要有一個合理的解答，因為問題的解法很多也沒有一定正確的，所以你需要更清楚自己的解法。
+        {{ $t('about_page.text') }}
       </p>
       <div class="res mb-8 col-sm-6">
         <figure class="figure mr-8">
@@ -21,92 +18,92 @@
             alt="..."
           />
         </figure>
-        <a href="https://drive.google.com/file/d/1NFNcFEzZj7I_Punp-17jKKBReY3QVjeK/view?usp=sharing" target="_blank">履歷瀏覽
+        <a href="https://drive.google.com/file/d/1NFNcFEzZj7I_Punp-17jKKBReY3QVjeK/view?usp=sharing" target="_blank">{{ $t('about_page.view_resume_cat') }}
         </a>
       </div>
     </section>
     <section class="time_line-h container">
-      <h2 class="mb-16">人生沒有白走的路，每一步都算數</h2>
+      <h2 class="mb-16">{{ $t('about_page.steps.title') }}</h2>
       <div class="timeline-h flex space-between">
         <div class="now">
           <p class="text-md">now - 2018</p>
           <div class="circle-grey mt-8 mb-8"></div>
-          <p class="text-md-bold">UI/UX 設計師</p>
-          <p class="text-sm">奧丁丁集團</p>
+          <p class="text-md-bold">{{ $t('common.uiux_designer') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.owlting') }}</p>
         </div>
         <div class="2018">
           <p class="text-md">2018 - 2017</p>
           <div class="circle-grey mt-8 mb-8"></div>
-          <p class="text-md-bold">網頁視覺設計師</p>
-          <p class="text-sm">蝦皮購物</p>
+          <p class="text-md-bold">{{ $t('about_page.steps.web_designer') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.shopee') }}</p>
         </div>
         <div class="2015">
           <p class="text-md">2017 - 2015</p>
           <div class="circle-grey mt-8 mb-8"></div>
-          <p class="text-md-bold">視覺設計師</p>
-          <p class="text-sm">樂子餐飲集團</p>
+          <p class="text-md-bold">{{ $t('about_page.steps.visual_designer') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.the_diner') }}</p>
         </div>
         <div class="2014">
           <p class="text-md">2015 - 2014</p>
           <div class="circle-grey mt-8 mb-8"></div>
-          <p class="text-md-bold">歐盟繪畫學士</p>
-          <p class="text-sm">瑞士 EPAC 當代藝術學院</p>
+          <p class="text-md-bold">{{ $t('about_page.steps.eu_bachelor') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.epac') }}</p>
         </div>
         <div class="2014">
           <p class="text-md">2013 - 2010</p>
           <div class="circle-grey mt-8 mb-8"></div>
-          <p class="text-md-bold">商業設計學系</p>
-          <p class="text-sm">銘傳大學</p>
+          <p class="text-md-bold">{{ $t('about_page.steps.cd') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.mcu') }}</p>
         </div>
       </div>
     </section>
     <section class="time_line-v container">
-      <h2 class="mb-16">人生沒有白走的路，每一步都算數</h2>
+      <h2 class="mb-16">{{ $t('about_page.steps.title') }}</h2>
       <div class="timeline-v column space-between">
         <div class="flex mb-24">
           <div class="circle-grey mt-14 mr-16"></div>
           <div class="now">
             <p class="text-md">now - 2018</p>
-            <p class="text-md-bold">UIUX設計師</p>
-            <p class="text-sm">奧丁丁集團</p>
+            <p class="text-md-bold">{{ $t('common.uiux_designer') }}</p>
+            <p class="text-sm">{{ $t('about_page.steps.owlting') }}</p>
           </div>
         </div>
         <div class="flex mb-24">
           <div class="circle-grey mt-14 mr-16"></div>
           <div class="now">
             <p class="text-md">2018 - 2016</p>
-            <p class="text-md-bold">網頁視覺設計師</p>
-            <p class="text-sm">蝦皮購物</p>
+            <p class="text-md-bold">{{ $t('about_page.steps.web_designer') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.shopee') }}</p>
           </div>
         </div>
         <div class="flex mb-24">
           <div class="circle-grey mt-14 mr-16"></div>
           <div class="now">
             <p class="text-md">2016 - 2015</p>
-            <p class="text-md-bold">視覺設計師</p>
-            <p class="text-sm">樂子美式餐飲</p>
+            <p class="text-md-bold">{{ $t('about_page.steps.visual_designer') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.the_diner') }}</p>
           </div>
         </div>
         <div class="flex mb-24">
           <div class="circle-grey mt-14 mr-16"></div>
           <div class="now">
             <p class="text-md">2015 - 2014</p>
-            <p class="text-md-bold">歐盟繪畫學士</p>
-            <p class="text-sm">留學瑞士EPAC</p>
+            <p class="text-md-bold">{{ $t('about_page.steps.eu_bachelor') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.epac') }}</p>
           </div>
         </div>
         <div class="flex">
           <div class="circle-grey mt-14 mr-16"></div>
           <div class="now">
             <p class="text-md">2013 - 2010</p>
-            <p class="text-md-bold">商業設計學系</p>
-            <p class="text-sm">奧丁丁集團</p>
+            <p class="text-md-bold">{{ $t('about_page.steps.cd') }}</p>
+          <p class="text-sm">{{ $t('about_page.steps.mcu') }}</p>
           </div>
         </div>
       </div>
     </section>
     <section class="skil_tree container">
-      <h2 class="mb-16">技能樹</h2>
+      <h2 class="mb-16">{{ $t('about_page.skill_tree.title') }}</h2>
       <div class="row mb-48">
         <div class="col-xl-2 col-md-3 col-4 text-align-center">
           <img
@@ -151,7 +148,7 @@
             style="width: 100px"
             alt="..."
           />
-          <p class="text-sm-bold text-align-center">JavaScript ( 學習中 )</p>
+          <p class="text-sm-bold text-align-center">JavaScript ( {{ $t('about_page.skill_tree.learning') }} )</p>
         </div>
         <div class="col-xl-2 col-md-3 col-4 text-align-center">
           <img
@@ -221,9 +218,9 @@
     <section class="NNgroup container">
       <div class="row mb-48">
         <div class="col-md-6 content-pr-48 mb-48">
-          <h2 class="mb-16">定期閱讀 NNgroup</h2>
+          <h2 class="mb-16">{{ $t('about_page.nngroup.title') }}</h2>
           <p class="text-md">
-            跟著大師走可以少走很多冤枉路，我也利用工程部技術交流會與同事們分享十大易用性的重要與實作。
+            {{ $t('about_page.nngroup.text') }}
           </p>
         </div>
         <div class="col-md-6 flex-center column content-pl-48">
@@ -267,9 +264,9 @@
     <section class="switzerland container">
       <div class="row mb-48">
         <div class="col-md-6 content-pr-48 mb-48">
-          <h2 class="mb-16">留學瑞士一年，擁有法文中級證書</h2>
+          <h2 class="mb-16">{{ $t('about_page.la_suisse.title') }}</h2>
           <p class="text-md">
-            在大四時留學瑞士一年並取得歐盟美術學士證書。回台灣花了三年的時間到台大進修部進修法文，在 2019 年取得法文B1鑑定證書，非常想再訪瑞士跟當時的同學當面用法文聊天。
+             {{ $t('about_page.la_suisse.text') }}
           </p>
         </div>
         <div class="col-md-6 flex-center column content-pl-48">
@@ -329,9 +326,9 @@
     <section class="side_project container">
       <div class="row">
         <div class="col-md-6 content-pr-48 mb-48">
-          <h2 class="mb-16">最想完成的 Side Project 是一款筋絡 APP</h2>
+          <h2 class="mb-16">{{ $t('about_page.side_project.title') }}</h2>
           <p class="text-md">
-            想做筋絡 APP 源自於重訓完使用筋膜槍放鬆的習慣，但常常會忘記該施力的穴道及強度，因此希望此款 APP 可以像客製化百科一樣來幫助使用者，為什麼是 APP 不是 Web？因為我還規劃了局部掃描穴位的功能，如果你對這個 APP 有興趣歡迎與我聯繫！
+            {{ $t('about_page.side_project.text') }}
           </p>
         </div>
         <div class="col-md-6 flex-center column content-pl-48">
@@ -384,7 +381,7 @@
       <div class="container">
         <div class="connection-area">
           <p class="text-md-bold mb-16 say-hi">
-            很期待認識你！有任何想法都歡迎隨時與我交流 ⭐
+            {{ $t('meeting.title') }}
           </p>
           <div class="row connect-way">
             <div class="mail mb-8 col-sm-6 content-pl-48">
