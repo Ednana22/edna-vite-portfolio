7ee665a95279db39d55bdea5ed0078cf9fd95bac
unick_how <unick.zhow@gmail.com>
2021-10-30 14:35:55 +0800

    feat: install lazyload

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
-import VueClickAway from "vue3-click-away"
+import VueClickAway from 'vue3-click-away'
+import VueLazyload from '@jambonn/vue-lazyload'
+
 
 import './styles/main.scss'
 
 const app = createApp(App)
+
 app
+  .use(VueLazyload)
   .use(VueClickAway)
   .use(Router)
   .mount('#app')
