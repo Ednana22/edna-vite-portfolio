18a6ace25cf3c251d8e627eff595a7a24e60f984
unick_how <unick.zhow@gmail.com>
2021-10-31 13:06:14 +0800

    feat: store lang

--- src/locales/index.js
+++ src/locales/index.js
@@ -2,8 +2,13 @@ import { createI18n } from 'vue-i18n'
 import zhTw from './lang/zh-tw.json'
 import en from './lang/en.json'
 
+import { useStorage } from '@vueuse/core'
+const lang = useStorage('edna_lang')
+
+if (!lang.value) lang.value = 'zh-tw'
+
 const i18n = createI18n({
-  locale: 'zh-tw',
+  locale: lang.value,
   fallbackLocale: 'zh-tw',
   globalInjection: true,
   legacy: false,
